aea073425f173a2430f753bc4aec653de5706264
artificerpi <artificerpi@users.noreply.github.com>
2017-11-18 15:24:39 +0800

    add gui test sample

--- src/test/java/wow/artificerpi/selenium/gui/BingTest.java
+++ src/test/java/wow/artificerpi/selenium/gui/BingTest.java
@@ -1,8 +1,17 @@
 package wow.artificerpi.selenium.gui;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class BingTest {
 
+	@Test
 	public void testOpenBingPage() {
-		WebDriver driver
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome(); 
+		WebDriver driver = new RemoteWebDriver(capabilities);
+		
+		driver.get("https://bing.com");
 	}
 }
